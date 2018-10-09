b4b41135d357f73be47548508fe187def1ef56dd
josephb1941 <joseph.blumenthal@medstar.net>
2018-10-09 14:04:40 -0500

    Update PRO.js

--- PRO.js
+++ PRO.js
@@ -15,12 +15,12 @@ function onReady(smart)  {
 	
 	var pt = patient.read();
 	
-	alert(JSON.stringify(pt));
+	alert(JSON.stringify(smart.patient.api));
 	
 	// var obv = smart.patient.api.fetchAll({
 				// type: 'Observation',
 				// query: {
-				  // code: {
+				  // // code: {
 					// $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
 						  // 'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
 						  // 'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
