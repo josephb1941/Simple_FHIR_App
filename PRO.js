6e10466c775a7cd0818a7b096289e8fb3b3586c9
josephb1941 <joseph.blumenthal@medstar.net>
2018-10-09 12:43:57 -0500

    Update PRO.js

--- PRO.js
+++ PRO.js
@@ -7,7 +7,7 @@ function onLoad() {
 
 function onReady(smart)  {
 	
-	alert("I'm ready!");
+	alert("Do I have it? " + smart.hasOwnProperty('patient'));
   // if (smart.hasOwnProperty('patient')) {
 	// var patient = smart.patient;
 	// var pt = patient.read();
